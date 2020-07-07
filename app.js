948878e4063f4a223b4b79a99e6702879ad50e4b
cjariasjr <46426735+christopherariasjr@users.noreply.github.com>
2020-07-07 12:43:24 -0700

    version 1.00.01 Adds the beginning to the cmd interface.

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-const token = process.env.TOKEN
-bot.on('ready', () => {
-    console.log("Bot is ready")
-})
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const token = process.env.TOKEN;
+bot.on("ready", () => {
+  console.log("Bot is ready");
+});
 
-bot.on('message', msg => {
-    if (msg.content == 'HELLO') {
-        msg.reply('SHHH.... This is a library.')
+bot.on("message", msg => {
+  sleep(1000).then(() => {
+    if (msg.content === "!jojo") {
+      msg.reply("");
     }
-})
 
+    if (msg.content === "!help") {
+      msg.reply("Use the !<cmd> to use bot");
+      msg.reply("!jojo: Gives you a Jojo meme.");
+    }
+  });
+});
 
-bot.login(token)
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+
+bot.login(token);
