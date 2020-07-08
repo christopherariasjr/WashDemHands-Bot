353122622a435fd4f097fe318b03c76718cc4ec6
Christopher Arias Jr <cjariasjr@pop-os.localdomain>
2020-07-08 09:15:39 -0700

    1.00.02

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
+require('dotenv').config()
 const token = process.env.TOKEN
 bot.on('ready', () => {
     console.log("Bot is ready")
 })
 
 bot.on('message', msg => {
-    if (msg.content == 'HELLO') {
-        msg.reply('SHHH.... This is a library.')
+
+    switch (msg.content) {
+        case 'HELLO':
+            msg.reply('SHHH.... This is a library.');
+        break;
+        case 'BITCH':
+            msg.reply('Nah, you a bitch.')
+        break;
+        case 'Fuck you' || 'FUCK YOU':
+            msg.reply('1 v 1 me in Smash... No items. ')
+
     }
 })
 
