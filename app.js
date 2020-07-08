f995b333b7a852733095a9afd4b9dd352d7540a7
Christopher Arias Jr <cjariasjr@pop-os.localdomain>
2020-07-08 09:20:31 -0700

    1.00.02

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 require('dotenv').config()
@@ -19,26 +18,7 @@ bot.on('message', msg => {
         case 'Fuck you' || 'FUCK YOU':
             msg.reply('1 v 1 me in Smash... No items. ')
 
-=======
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const token = process.env.TOKEN;
-bot.on("ready", () => {
-  console.log("Bot is ready");
-});
-
-bot.on("message", msg => {
-  sleep(1000).then(() => {
-    if (msg.content === "!jojo") {
-      msg.reply("");
     }
-
-    if (msg.content === "!help") {
-      msg.reply("Use the !<cmd> to use bot");
-      msg.reply("!jojo: Gives you a Jojo meme.");
->>>>>>> 53cc66942f362f33398a253543f865daaacdd130
-    }
-  });
 });
 
 function sleep(time) {
